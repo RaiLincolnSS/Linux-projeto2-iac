01bf66ab5dd60597c1431dab180bc8d8460b83a9
RaiLincolnSS <developer.rai30@gmail.com>
2022-11-13 21:01:41 -0300

    Arquivos IaC v.1.0

---
+++ Exibir_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+echo "Exibindo data atual..."
+date
---
+++ criando_usuarios.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Criando usuarios do sistema..."
+
+useradd guest10 -c "Usuario convidado" -s /bin/bash -m -p $(openssl passwd  -crypt senha123)
+passwd guest10 -e
+
+useradd guest11 -c "Usuario convidado" -s /bin/bash -m -p $(openssl passwd  -crypt senha123)
+passwd guest11 -e
+
+useradd guest12 -c "Usuario convidado" -s /bin/bash -m -p $(openssl passwd  -crypt senha123)
+passwd guest12 -e
+
+useradd guest13 -c "Usuario convidado" -s /bin/bash -m -p $(openssl passwd  -crypt senha123)
+passwd guest13 -e
+
+useradd guest14 -c "Usuario convidado" -s /bin/bash -m -p $(openssl passwd  -crypt senha123)
+passwd guest14 -e
+
+
+echo "Finalizando!!!"
